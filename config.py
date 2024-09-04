8f2854ca4352b4b60c938af615dbc6b8240e9525
Semen Saifutdinov <saifutdinov2006@mail.ru>
2024-09-04 12:36:39 +0300

    Update config

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
 from dotenv import find_dotenv, load_dotenv
 import os
 
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 if not find_dotenv():
     exit("Не загружены необходимые переменные окружения!")
 else:
     load_dotenv()
+
+
+API_URL = os.getenv("API_URL")
+MEDIA_URL = os.getenv("MEDIA_URL")
+API_TOKEN_INSTANCE = os.getenv("API_TOKEN_INSTANCE")
+ID_INSTANCE = os.getenv("ID_INSTANCE")
 
